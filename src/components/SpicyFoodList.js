e1d3ac9227649792707327e3faba1e7f9204131d
Chloe <chloepeng331@icloud.com>
2022-02-24 23:44:47 -0500

    commit tree 116d8a0ff6e6eab124fa958f203e0db64e93c65a

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -7,20 +7,49 @@ function SpicyFoodList() {
   function handleAddFood() {
     const newFood = getNewSpicyFood();
     console.log(newFood);
+
+    const updateFood = [...foods, newFood]
+    setFoods(updateFood);
   }
 
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id}> onClick={()=> handleLiClick(food.id)}
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
 
+  function handleLiClick(id) {
+    const updatedFoods = foods.map((food) => {
+      [1, 2, 3].filter((number) => number !== 3);
+      if (food.id === id) {
+          return { ...food, heatLevel: food.heatLevel + 1, }
+      } else {
+         return food 
+      }
+    })
+    setFoods(updatedFoods)
+  }
+
+  
+  
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
     </div>
   );
+
+
 }
 
+
 export default SpicyFoodList;
+
+// const updatedTodos = todos.map(todo => {
+//   if (todo.id === id) {
+//       return { ...todo, completed }
+//   } else {
+//      return todo 
+//   }
+// })
+// setTodos(updatedTodos)
